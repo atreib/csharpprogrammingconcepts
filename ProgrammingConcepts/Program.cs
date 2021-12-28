3fd032eac023ddb514a28ad3aef11a9474eee17c
Andre Treib <andre.schumacher.treib@gmail.com>
2021-12-27 22:01:39 -0300

    oo: encapsulation

---
+++ ProgrammingConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ProgrammingConcepts.oo;
+
+namespace ProgrammingConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulation example = new Encapsulation(100);
+            Console.WriteLine(example.isEnough());
+            example.add(200);
+            Console.WriteLine(example.isEnough());
+            // example.teste; // it's not going to compile
+        }
+    }
+}
---
+++ ProgrammingConcepts/oo/Encapsulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ProgrammingConcepts.oo
+{
+    public class Encapsulation
+    {
+        private int minimum;
+        private int total = 0;
+
+        public Encapsulation(int initialMinimum)
+        {
+            this.minimum = initialMinimum;
+        }
+
+        public bool isEnough()
+        {
+            return this.total > this.minimum;
+        }
+
+        public void add(int value)
+        {
+            this.total += value;
+        }
+    }
+
+    class EncapsulationExample
+    {
+        static void teste()
+        {
+            Encapsulation example = new Encapsulation(100);
+            Console.WriteLine(example.isEnough());
+            example.add(200);
+            Console.WriteLine(example.isEnough());
+            // example.teste; // it's not going to compile
+        }
+    }
+}
